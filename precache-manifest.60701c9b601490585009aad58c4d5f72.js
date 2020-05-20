4de35cceff7604965be7d54a2ed0cc4f8977303b
Harsh-Khurana <harshkhuranaharshkh@icloud.com>
2020-05-20 21:09:56 +0530

    Updates

---
+++ precache-manifest.60701c9b601490585009aad58c4d5f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a960f4755fad11b32c5ffd49f651d7",
+    "url": "/Kanban-board-demo/index.html"
+  },
+  {
+    "revision": "0ea19a1400ed12214835",
+    "url": "/Kanban-board-demo/static/css/main.445b38bf.chunk.css"
+  },
+  {
+    "revision": "06f77932c0fa5e6ff587",
+    "url": "/Kanban-board-demo/static/js/2.9c07782c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kanban-board-demo/static/js/2.9c07782c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea19a1400ed12214835",
+    "url": "/Kanban-board-demo/static/js/main.9ffb2436.chunk.js"
+  },
+  {
+    "revision": "7a4396b9678e93255e88",
+    "url": "/Kanban-board-demo/static/js/runtime-main.446e63f2.js"
+  }
+]);
